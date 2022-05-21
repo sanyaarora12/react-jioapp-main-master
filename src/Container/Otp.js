43c0428f1e18a624cb321f6f8606031c5cb7edd1
sanyaarora12 <arorasanya2712@gmail.com>
2022-05-21 15:58:15 +0530

    otp

--- src/Container/Otp.js
+++ src/Container/Otp.js
@@ -15,18 +15,19 @@ export default function Otp() {
         <div>
           <OtpStyle/>
           <div className="container">
-              <h2>Otp has been sent successfully. <br/> Please enter the same OTP below</h2>
-              <div className="userInput">
-                  <input type="text" id='ist' maxLength="1" onkeyup="clickEvent(this,'sec')"></input>
+              <h2>Otp has been sent successfully. <br/> please enter the same OTP below</h2>
+              <br />
+              <div class="hstack gap-3" >
+                  <input type="text" id='sec' maxLength="1" onkeyup="clickEvent(this,'sec')"></input>
                   <input type="text" id='sec' maxLength="1" onkeyup="clickEvent(this, 'third')"></input>
-                  <input type="text" id='third' maxLength="1" onkeyup="clickEvent(this, 'fourth')"></input>
-                  <input type="text" id='fourth' maxLength="1" onkeyup="clickEvent(this, 'fifth')"></input>
-                  <input type="text" id='fifth' maxLength="1"></input>
+                  <input type="text" id='sec' maxLength="1" onkeyup="clickEvent(this, 'fourth')"></input>
+                  <input type="text" id='sec' maxLength="1" onkeyup="clickEvent(this, 'fifth')"></input>
+                  <input type="text" id='sec' maxLength="1"></input>
           </div>
-          <br/> <br/> <br/> <br/>
+          <br />
         
           <button type="button" className="btn btn-primary btn-lg">
-            <Link to="/kycpage"
+            <Link to="/kycpage" id="submit"
             style={{ textDecoration: "none", color: "White" }}>
             Submit
             </Link>
