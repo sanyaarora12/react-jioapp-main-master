0ca545d09a9635c7be8e50e886ce01da380f6dee
sanyaarora12 <arorasanya2712@gmail.com>
2022-05-21 22:20:55 +0530

    dd

--- src/Container/Otp.js
+++ src/Container/Otp.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import OtpStyle from "../Components/OtpStyle";
 
-
 export default function Otp() {
    
     function clickEvent(first,last){
