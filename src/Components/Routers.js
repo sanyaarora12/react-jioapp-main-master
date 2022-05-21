d3b671b442b9ab115f02201d523b6b9509cf5b5b
sanyaarora12 <arorasanya2712@gmail.com>
2022-05-21 15:39:10 +0530

    sat

--- src/Components/Routers.js
+++ src/Components/Routers.js
@@ -2,11 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "../Container/Header";
 import Home from "../Container/Home";
-import Page from "../Container/Page";
 import Terms from "../Container/Terms";
-import Login from "../Container/Login"
 import ValidatedLoginForm from "../Container/ValidatedLoginForm";
 import { LoginForm } from "../Container/LoginForm";
+import KycPage from "../Container/KycPage";
 
 export default function Routers () {
   return (
@@ -16,10 +15,9 @@ export default function Routers () {
             <Route exact path="/" element={<Home />} />
             <Route path="/header" element={<Header />} />
             <Route path="/terms" element={<Terms />} />
-            <Route path="/page" element={<Page />} />
-            <Route path="/Login" element={<Login />} />
             <Route path="/ValidatedLoginForm" element={<ValidatedLoginForm/>} />
             <Route path="/LoginForm" element={<LoginForm />} />
+            <Route path="/KycPage" element={<KycPage />} />
           </Routes>
         </Router>
     </div>
--- src/Container/ValidatedLoginForm.js
+++ src/Container/ValidatedLoginForm.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Formik } from "formik";
 import * as EmailValidator from "email-validator";
 import * as Yup from "yup";
 import Button from "../Components/Button";
 const ValidatedLoginForm = () => (
   <Formik
-    initialValues={{ email: "", Phonenumber: "",shopname:"" }}
+    initialValues={{ email: "", Phonenumber: "", shopname: "" }}
     onSubmit={(values, { setSubmitting }) => {
       setTimeout(() => {
         console.log("Logging in", values);
@@ -13,7 +14,7 @@ const ValidatedLoginForm = () => (
       }, 500);
     }}
     //********Handling validation messages yourself*******/
-    validate={values => {
+    validate={(values) => {
       let errors = {};
       if (!values.email) {
         errors.email = "Required";
@@ -24,15 +25,15 @@ const ValidatedLoginForm = () => (
       const PhonenumberRegex = /(?=.*[0-10])/;
       if (!values.Phonenumber) {
         errors.Phonenumber = "Required";
-      } else if (values.Phonenumber.length ===11) {
+      } else if (values.Phonenumber.length === 11) {
         errors.Phonenumber = "Phone number must be 10 digits only.";
       } else if (!PhonenumberRegex.test(values.Phonenumber)) {
-        errors.Phonenumber = "Invalida Phonenumber";
+        errors.Phonenumber = "Invalida Phonenumber. Must contain one number";
       }
 
       if (!values.shopname) {
         errors.shopname = "Required";
-      } else if(values.shopname==="text" ){
+      } else if (values.shopname === "text") {
         errors.shopname = "Invalid shopname";
       }
 
@@ -41,16 +42,14 @@ const ValidatedLoginForm = () => (
     //********Using Yum for validation********/
 
     validationSchema={Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required("Required"),
+      email: Yup.string().email().required("Required"),
       Phonenumber: Yup.string()
         .required("No Phonenumber provided.")
         .min(8, "Phonenumber is too short - should be 10 digits minimum.")
-        .matches(/(?=.*[0-9])/, "Phonenumber must contain a number.")
+        .matches(/(?=.*[0-9])/, "Phonenumber must contain a number."),
     })}
   >
-    {props => {
+    {(props) => {
       const {
         values,
         touched,
@@ -58,66 +57,91 @@ const ValidatedLoginForm = () => (
         isSubmitting,
         handleChange,
         handleBlur,
-        handleSubmit
+        handleSubmit,
       } = props;
       return (
         <>
-        <Button />
-        <h1>Register to Jio Mart Digital</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-          <label htmlFor="email">Email</label><br />
-          <input
-            name="email"
-            type="text"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email && "error"}
-          />
-          {errors.email && touched.email && (
-            <div className="input-feedback">{errors.email}</div>
-          )}
-          </div>
-          <div>
-          <label htmlFor="email">Phone Number</label><br />
-          <input
-            name="Phonenumber"
-            type="Phonenumber"
-            placeholder="Enter your Phonenumber"
-            value={values.Phonenumber}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.Phonenumber && touched.Phonenumber && "error"}
-          />
-          {errors.Phonenumber && touched.Phonenumber && (
-            <div className="input-feedback">{errors.Phonenumber}</div>
-          )}
-          </div>
-          <div>
-          <label htmlFor="email">Shop Name</label>
-          <br />
-          <input
-            name="shopname"
-            type="shopname"
-            placeholder="Enter your Shop Name"
-            value={values.shopname}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.shopname && touched.shopname && "error"}
-          />
-          {errors.shopname && touched.shopname && (
-            <div className="input-feedback">{errors.shopname}</div>
-          )}
-          </div>
-          {/* <label>Shop name</label>
+          <Button />
+          <h1>Register to Jio Mart Digital</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <br />
+              <input
+                id="placeholder"
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email && "error"}
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="email">Phone Number</label>
+              <br />
+              <input
+                id="placeholder"
+                name="Phonenumber"
+                type="Phonenumber"
+                placeholder="Enter your Phone number"
+                value={values.Phonenumber}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.Phonenumber && touched.Phonenumber && "error"}
+              />
+              {errors.Phonenumber && touched.Phonenumber && (
+                <div className="input-feedback">{errors.Phonenumber}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="email">Shop Name</label>
+              <br />
+              <input
+                id="placeholder"
+                name="shopname"
+                type="shopname"
+                placeholder="Enter your Shop Name"
+                value={values.shopname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.shopname && touched.shopname && "error"}
+              />
+              {errors.shopname && touched.shopname && (
+                <div className="input-feedback">{errors.shopname}</div>
+              )}
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="invalidCheck"
+                    required
+                  />
+                  <label className="form-check-label" for="invalidCheck">
+                    Agree to terms and conditions
+                  </label>
+                  <div className="invalid-feedback">
+                    You must agree before submitting.
+                  </div>
+                </div>
+              </div>
+           
+            {/* <label>Shop name</label>
           <input type="text" placeholder="Enter your shop name"/> */}
-          <br/>
-          <button type="submit" disabled={isSubmitting}>
-            Login
-          </button>
-        </form>
+            <br />
+            <button type="submit" disabled={isSubmitting}>
+              <Link
+                to="/kycpage"
+                style={{ textDecoration: "none", color: "White" }}
+              >
+                Login
+              </Link>
+            </button>
+          </form>
         </>
       );
     }}
