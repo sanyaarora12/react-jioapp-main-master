59a90ede067032c69e12aee5416092c2d5c0d240
Sanyaarora12 <Arorasanya2712@gmail.com>
2022-05-25 03:28:45 +0530

    ad

--- src/Container/Address.js
+++ src/Container/Address.js
@@ -1,5 +1,6 @@
 import React from "react";
 import KycStyle from "../Components/KycStyle";
+import { Link } from "react-router-dom";
 
 export class Address extends React.Component {
   constructor(props) {
@@ -23,6 +24,7 @@ export class Address extends React.Component {
           name="PinCode"
           type="number"
           placeholder="Enter your PinCode"
+          required="true"
           value={PinCode}
           onChange={this.handleChange}
         />
@@ -34,12 +36,32 @@ export class Address extends React.Component {
           name="Address"
           type="Address"
           placeholder="Enter your Address"
+          required="true"
           value={Address}
           onChange={this.handleChange}
         />
         <br />
         <br />
-        <button type="submit">Login</button>
+        <div class="mb-3" id="a">
+          <label for="formFileSm" class="form-label">
+            Upload Address Proof
+          </label>
+          <input
+            class="form-control form-control-sm"
+            id="formFileSm"
+            type="file"
+            required
+          />
+        </div>
+        <br />
+        <button type="submit">
+          <Link
+            to="/validatedbankdetails"
+            style={{ textDecoration: "none", color: "White" }}
+          >
+            Submit
+          </Link>
+        </button>
       </form>
     );
   }
