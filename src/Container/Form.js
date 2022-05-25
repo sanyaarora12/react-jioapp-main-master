e6477a0dbc2159e2bf0b3de5e15d1011e8968dbf
sanyaarora12 <arorasanya2712@gmail.com>
2022-05-25 17:30:47 +0530

    k

--- src/Container/Form.js
+++ src/Container/Form.js
@@ -51,7 +51,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://www.solidbackgrounds.com/images/2560x1440/2560x1440-blue-solid-color-background.jpg)',
+            backgroundImage: 'url(https://wallpaperaccess.com/full/2758365.png)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
