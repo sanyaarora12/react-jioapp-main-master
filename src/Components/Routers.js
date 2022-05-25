23192c752e1228e66d1a9bd4a62bd27cec75f0df
Sanyaarora12 <Arorasanya2712@gmail.com>
2022-05-25 17:22:31 +0530

    del

--- src/Components/Routers.js
+++ src/Components/Routers.js
@@ -12,13 +12,8 @@ import Otp from "../Container/Otp";
 import { BankDetails } from "../Container/BankDetails";
 import ValidatedBankDetails from "../Container/ValidatedBankDetails";
 import SuccessfulRegistration from "../Container/SuccessfulRegistration";
-<<<<<<< HEAD
 import Form from "../Container/Form";
 import Kycform from "../Container/KycForm";
-=======
-import  Form  from "../Container/Form";
-import  Form3  from "../Container/Form3";
->>>>>>> d286764e2cf5f8fcae171fae424b3fbae54fd624
 
 export default function Routers() {
   return (
@@ -32,12 +27,10 @@ export default function Routers() {
           <Route path="/terms" element={<Terms />} />
           <Route path="/ValidatedLoginForm" element={<ValidatedLoginForm />} />
           <Route path="/LoginForm" element={<LoginForm />} />
-          <Route path="/KycPage" element={<KycPage />} />
-          <Route path="/ValidatedKyc" element={<ValidatedKyc />} />
-          <Route path="/Address" element={<Address />} />
+          
           <Route path="/otp" element={<Otp />} />
           <Route path="/Kycform" element={<Kycform />} />
-          <Route path="/bankdetails" element={<BankDetails />} />
+          
           <Route
             path="/validatedbankdetails"
             element={<ValidatedBankDetails />}
