ce81b70696d020c277316e7008371483d7996d44
Sanyaarora12 <Arorasanya2712@gmail.com>
2022-07-06 11:10:44 +0530

    json

--- src/Container/Approval.js
+++ src/Container/Approval.js
@@ -1,52 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import data from "./data.json";
 
 export default function Approval() {
-  const [contacts,setContacts]=useState(data);
-  
+  const [contacts, setContacts] = useState(data);
+
   const handleRemove = (id) => {
     const arr = contacts.filter((item) => item.id !== id);
     setContacts(arr);
   };
 
-  const handleClick=()=>{
-    alert("accepted!")
-  }
+  const handleClick = () => {
+    alert("accepted!");
+  };
   return (
-    <div id='output'>
-        <table>
-            <thead>
-              <tr >
-                <th>Full Name</th>
-                <th>Address</th>
-                <br/>
-                <th>Phone number</th>
-                <br/>
-                <th>Email</th>
-                <br/>
-                <th>Pan number</th>
-                
-                <th>Aadhaar number</th>
-              </tr>
-            </thead>
-            <tbody>
-              {contacts.map((contact,index)=>(
-                <tr>
-                  <td>{contact.fullName}</td>
-                  <br/>
-                  <td>{contact.address}</td>
-                  <br/>
-                  <td>{contact.phoneNumber}</td>
-                  <br/><br/>
-                  <td>{contact.email}</td>
-                  <td>{contact.pannumber}</td>
-                  <td>{contact.aadhaarno}</td>
-                  <button onClick={handleClick}>Accept</button>
-                  <button onClick={() => handleRemove(contact.id)}>Reject</button>
-                </tr>
-              ))}
-            </tbody>
-        </table>
+    <div id="output">
+      <h1 style={{ color: "darkblue", marginLeft: "500px" }}>
+        {" "}
+        Admin Approval{" "}
+      </h1>
+      <table class="table table-light">
+        <thead>
+          <tr>
+            <th scope="col">Full Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone number</th>
+            <th scope="col">Email</th>
+            <th scope="col">Pan Number</th>
+            <th scope="col">Adhaar Number</th>
+            <th scope="col">GST Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => (
+            <tr>
+              <td>{contact.fullName}</td>
+
+              <td>{contact.address}</td>
+
+              <td>{contact.phoneNumber}</td>
+
+              <td>{contact.email}</td>
+              <td>{contact.pannumber}</td>
+              <td>{contact.aadhaarno}</td>
+              <td>{contact.GSTNumber}</td>
+
+              <button id="green" onClick={handleClick}>
+                Accept
+              </button>
+              <button id="red" onClick={() => handleRemove(contact.id)}>
+                Reject
+              </button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
